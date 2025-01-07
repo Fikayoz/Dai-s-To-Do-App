b1c7acad54abbde620d8aabc5dbb95af8a22e402
Fikayo <Fikayoz@gmail.com>
2025-01-06 23:43:16 -0500

    Completed all app functionalities. For VS code, will need live server for local storage segment

--- script.js
+++ script.js
@@ -14,15 +14,30 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         // If the target element's class is empty, it adds "checked". If "checked" is there, it removes the class name
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if(e.target.tagName === "SPAN"){
         // Deletes list item element entirely if the span element is clicked
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
+
+
+// functions to save and retrieve saved local storage data to persist data between reloads
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
